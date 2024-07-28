c3b6b7d40d4966739f2cd315990febb429cbf566
Jane bebik <118847459+jane-bebik@users.noreply.github.com>
2024-07-28 16:14:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const catFacts = [
     { fact: "Cats are one of, if not the most, popular pet in the world." },
     { fact: "Cats have five toes on each front paw, but only four toes on each back paw." },
     { fact: "Cats can rotate their ears 180 degrees." },
-    // Add more facts as needed
+    
 ];
 
 app.get('/api/items', (req, res) => {
